8c04af19693c200650608d25721d2d9550e971e2
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-01-02 23:30:40 +0300

    Add platform.

--- game.js
+++ game.js
@@ -1,18 +1,31 @@
 const game = {
+  width: 640,
+  height: 360,
+  ctx: undefined,
+  platform: undefined,
   sprites: {
-    background: undefined
+    background: undefined,
+    platform: undefined
   },
-  start: function() {
+  init: function() {
     const canvas = document.getElementById('mycanvas');
     this.ctx = canvas.getContext('2d');
-
-    this.sprites.background = new Image();
-    this.sprites.background.src = 'images/background.png';
-
+  },
+  load: function() {
+    for(let key in this.sprites) {
+      this.sprites[key] = new Image();
+      this.sprites[key].src = `images/${key}.png`;
+    }
+  },
+  start: function() {
+    this.init();
+    this.load();
     this.run();
   },
   render: function() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
     this.ctx.drawImage(this.sprites.background, 0, 0);
+    this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
   },
   run: function() {
     this.render();
@@ -21,6 +34,11 @@ const game = {
       game.run();
     });
   }
+};
+
+game.platform = {
+  x: 300,
+  y: 300
 };
 
 window.addEventListener('load', function() {
